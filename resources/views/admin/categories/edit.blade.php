8549e4de13c9ed0be3786ae66675096bb7820de9
W33bEE <bulthe_wayne@hotmail.com>
2019-12-01 18:55:54 +0100

    edit afwerken !

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,1 +1,15 @@
-<?php
+@extends('layouts.admin')
+@section('dash')
+    Edit category
+    @stop
+@section('content')
+    <div class="col-md-6">
+        {!! Form::model($category,['method'=>'PATCH','action'=>['AdminCategoriesController@update',$category->id]]) !!}
+        <div class="form-group">
+            {!! Form::label('name','Name:') !!}
+            {!! Form::text('name',null,['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group"></div>
+        <div class="form-group"></div>
+    </div>
+    @stop
